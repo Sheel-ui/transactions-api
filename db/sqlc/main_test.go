b0f4cd74bb6ed14b7296f891961483f48eb896a6
Sheel-ui <sheeltaskar64@gmail.com>
2024-06-22 17:10:09 +0530

    init viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,21 +7,21 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/transactions?sslmode=disable"
+	"github.com/sheel-ui/transactions-api/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
 	if err != nil {
-		log.Fatal("cant connect to db:", err)
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
 	}
 
 	testQueries = New(testDB)
