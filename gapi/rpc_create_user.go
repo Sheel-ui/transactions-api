cc318c0b58d6282e93c97bd3fb8d9f215208f256
Sheel-ui <sheeltaskar64@gmail.com>
2024-06-28 16:04:33 +0530

    asyncq server intergation

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -2,12 +2,15 @@ package gapi
 
 import (
 	"context"
+	"time"
 
+	"github.com/hibiken/asynq"
 	"github.com/lib/pq"
 	db "github.com/sheel-ui/transactions-api/db/sqlc"
 	"github.com/sheel-ui/transactions-api/pb"
 	"github.com/sheel-ui/transactions-api/util"
 	val "github.com/sheel-ui/transactions-api/validator"
+	"github.com/sheel-ui/transactions-api/worker"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -41,6 +44,20 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 			}
 		}
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	// TODO: use db transaction
+	taskPayload := &worker.PayloadSendVerifyEmail{
+		Username: user.Username,
+	}
+	opts := []asynq.Option{
+		asynq.MaxRetry(10),
+		asynq.ProcessIn(10 * time.Second),
+		asynq.Queue(worker.QueueCritical),
+	}
+	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to distribute task to send verify email: %s", err)
 	}
 
 	rsp := &pb.CreateUserResponse{
